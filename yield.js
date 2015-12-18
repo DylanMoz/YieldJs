c10dc2b1f1bb40223c869af1aa0c54891d72d8e4
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2015-12-18 15:24:05 -0800

    Added functions compact, concat, consecutive, contains, countBy, dropWhile, each, every, find, and first. Need tests.

--- yield.js
+++ yield.js
@@ -43,6 +43,127 @@
       });
     }
 
+    compact () {
+      const that = this;
+      return new Yield(function* fn () {
+        for (let v of that.generator()) {
+          if (v) {
+            yield v;
+          }
+        }
+      });
+    }
+
+    concat (...iterables) {
+      const that = this;
+      return new Yield(function* fn () {
+        for (let v of that.generator()) {
+            yield v;
+        }
+        for (let iterable of iterables) {
+          for (let v of iterable) {
+            yield v;
+          }
+        }
+      });
+    }
+
+    compact () {
+      const that = this;
+      return new Yield(function* fn () {
+        for (let v of that.generator()) {
+          if (v) {
+            yield v;
+          }
+        }
+      });
+    }
+
+    consecutive (num) {
+      if (num <= 0)
+        return new Yield([]);
+
+      const that = this;
+      return new Yield(function* fn () {
+        let firstReturned = false;
+        let arr = [];
+        for (let v of that.generator()) {
+          arr.push(v);
+          if (!firstReturned) {
+            if (arr.length === num) {
+              yield arr;
+              firstReturned = true;
+            }
+          } else {
+            yield (arr = arr.slice(1));
+          }
+        }
+
+        if (firstReturned) {
+          yield arr;
+        }
+      });
+    }
+
+    contains (value) {
+      for (let v of this.generator()) {
+        if (v === value) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    countBy (fn) {
+      let hash = {};
+      let key;
+      for (let v of this.generator()) {
+        key = fn(v);
+        if (key in hash) {
+          hash[key]++;
+        } else {
+          hash[key] = 0;
+        }
+      }
+      return hash;
+    }
+
+    dropWhile (fn) {
+      const that = this;
+      return new Yield(function* fn () {
+        let dropping = true;
+        for (let v of that.generator()) {
+          if (dropping) {
+            if (!fn(v)) {
+              dropping = false;
+              yield v;
+            }
+          } else {
+            yield v;
+          }
+        }
+      });
+    }
+
+    each (fn) {
+      for (let v of this.generator()) {
+        let result = fn(v);
+        if (!result) {
+          break;
+        }
+      }
+      return this;
+    }
+
+    every (fn) {
+      for (let v of this.generator()) {
+        if (!fn(v)) {
+          return false;
+        }
+      }
+      return true;
+    }
+
     filter (lambda) {
       const that = this;
       return new Yield(function* fn () {
@@ -52,6 +173,35 @@
             if (result) { // Truthy values are accepted
               yield v;
             }
+        }
+      });
+    }
+
+    find (fn) {
+      for (let v of this.generator()) {
+        if (fn(v)) {
+          return v;
+        }
+      }
+    }
+
+    first (num) {
+      const that = this;
+      return new Yield(function* fn () {
+        let skip = 0;
+        if (!num) {
+          let v = that.generator().next();
+          if (!v.done) {
+            yield v.value;
+          }
+          return;
+        }
+        for (let v of that.generator()) {
+          if (num-- > 0) {
+            yield v;
+          } else {
+            break;
+          }
         }
       });
     }
@@ -69,6 +219,7 @@
     }
 
     reverse () {
+      // CANNOT be used on infinite generators.
       const that = this;
       return new Yield(function* fn () {
         const arr = [...that.iterable];
